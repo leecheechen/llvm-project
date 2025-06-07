53b5524f78f337b82fe2ad9e344f7f294e65d8b6
chenli <chenli@loongson.cn>
2025-06-07 10:50:43 +0800

    [llvm-cvtres] Add support for loongarch64

--- llvm/lib/Object/WindowsResource.cpp
+++ llvm/lib/Object/WindowsResource.cpp
@@ -992,6 +992,9 @@ void WindowsResourceCOFFWriter::writeFirstSectionRelocations() {
     case Triple::aarch64:
       Reloc->Type = COFF::IMAGE_REL_ARM64_ADDR32NB;
       break;
+    case Triple::loongarch64:
+      Reloc->Type = COFF::IMAGE_REL_LARCH_ADDR32NB;
+      break;
     default:
       llvm_unreachable("unknown machine type");
     }
